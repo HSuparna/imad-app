71647a2ab309a6e18e87dad5f486429607346371
HSuparna <30805422+HSuparna@users.noreply.github.com>
2017-08-22 20:18:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto = require('crypto');
 
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash (input,salt) {
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf", "10000", salt, hashed.toString('hex')].join('$');
+}
+app.get('/hash/:input', function (req, res) {
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
 });
 
 app.get('/article-one' , function (req, res) {
