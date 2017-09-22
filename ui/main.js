c818398c89343ca3976cf531be65104932de5c08
HSuparna <30805422+HSuparna@users.noreply.github.com>
2017-09-22 20:06:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,1 @@
-//console.log('Loaded!');
-var img = document.getElementById('/ui/madi.png');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight,50);
-};
+console.log('Loaded!');
